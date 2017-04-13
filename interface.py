3898979d915b8b2fceef5fbf3e9cbfdadd16b5c4
Raunak Sinha <raunak15075@iiitd.ac.in>
2017-04-13 23:25:03 +0530

    Second Commit

--- interface.py
+++ interface.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from flaskext.mysql import MySQL
  
 mysqlInstance = MySQL()
@@ -9,9 +9,23 @@ interface.config['MYSQL_DATABASE_DB'] = 'ques2'
 interface.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysqlInstance.init_app(interface)
  
-@interface.route("/")
+@interface.route("/") #This is the main function of intrerface app
 def hello():
     return "App is just a variable "
  
 if __name__ == "__main__":
     interface.run()
+
+@interface.route("/Authenticate")
+def Authenticate():
+        #username = request.args.get('UserName')
+        #password = request.args.get('Password')
+        cursor = mysqlInstance.connect().cursor()
+        cursor.execute("SELECT * from cars where cid = '20001' ")
+        data = cursor.fetchone()
+        if data is None:
+            return "Username or Password is wrong"
+        else:
+            print(data)
+            return "HAHAHA"
+
