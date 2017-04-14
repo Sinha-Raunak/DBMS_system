439407d36e001b22ba276369e7e5ece5883ffb21
Raunak Sinha <raunak15075@iiitd.ac.in>
2017-04-14 10:49:36 +0530

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,6 +1,9 @@
-from flask import Flask,request
+from flask import Flask
+from flask import render_template
+from flask import request
 from flaskext.mysql import MySQL
- 
+#from flask.ext.appbuilder import Model
+
 mysqlInstance = MySQL()
 interface = Flask(__name__)
 interface.config['MYSQL_DATABASE_USER'] = 'raunaksinha'
@@ -10,11 +13,24 @@ interface.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysqlInstance.init_app(interface)
  
 @interface.route("/") #This is the main function of intrerface app
-def hello():
-    return "App is just a variable "
- 
+def form_for_query_type(): #First Form
+    return render_template("firstForm.html")
+    
 if __name__ == "__main__":
     interface.run()
+
+@interface.route('/A',methods=['POST'])
+def handler_first_form():
+    print("reached Here")
+    text = request.form.get('t')
+    cursor = mysqlInstance.connect().cursor()
+    cursor.execute(" select distinct dname from driver NATURAL JOIN cars NATURAL JOIN reserves where cname = %s",text)
+    data = cursor.fetchall()
+    print(data)
+    
+    #print(data)
+    #print("THIS IS TEXT" + text)
+    return str(data)
 
 @interface.route("/Authenticate")
 def Authenticate():
